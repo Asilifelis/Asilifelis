072bbd07cea021b0bd8fe4f25881dcb8b4cbbaa2
Mia Winter <mia@miawinter.de>
2024-10-02 22:39:24 +0200

    added icon to actor view

--- Asilifelis/Models/View/ActorView.cs
+++ Asilifelis/Models/View/ActorView.cs
@@ -2,13 +2,14 @@
 
 namespace Asilifelis.Models.View;
 
-public record ActorView(Uri Id, ActorType Type, string Name, string PreferredUsername, string Summary, Uri Inbox, Uri Outbox)
+public record ActorView(Uri Id, ActorType Type, string Name, Uri Icon, string PreferredUsername, string Summary, Uri Inbox, Uri Outbox)
 	: ActivityStreamsObject {
 	public ActorView(Uri baseUri, Actor actor)
 		: this(
 			new Uri(baseUri, $"api/actor/{actor.Id}"),
 			actor.Type,
 			actor.DisplayName,
+			new Uri(baseUri, "/media/default.jpg"),
 			actor.Username,
 			"Not implemented yet",
 			new Uri(baseUri, $"api/actor/{actor.Id}/inbox"),
