1bef9ad4a093684b7dda671a624f82ad2273dcf0
Mia Winter <mia@miawinter.de>
2024-10-02 22:46:22 +0200

    fixed Image

--- Asilifelis/Models/View/ActorView.cs
+++ Asilifelis/Models/View/ActorView.cs
@@ -2,14 +2,15 @@
 
 namespace Asilifelis.Models.View;
 
-public record ActorView(Uri Id, ActorType Type, string Name, Uri Icon, string PreferredUsername, string Summary, Uri Inbox, Uri Outbox)
+public record ActorView(Uri Id, ActorType Type, string Name, ImageView Icon, ImageView Image, string PreferredUsername, string Summary, Uri Inbox, Uri Outbox)
 	: ActivityStreamsObject {
 	public ActorView(Uri baseUri, Actor actor)
 		: this(
 			new Uri(baseUri, $"api/actor/{actor.Id}"),
 			actor.Type,
 			actor.DisplayName,
-			new Uri(baseUri, "/media/default.jpg"),
+			new ImageView(new Uri(baseUri, "/media/default.jpg")),
+			new ImageView(new Uri(baseUri, "/media/default.jpg")),
 			actor.Username,
 			"Not implemented yet",
 			new Uri(baseUri, $"api/actor/{actor.Id}/inbox"),
---
+++ Asilifelis/Models/View/ImageView.cs
@@ -0,0 +1,3 @@
+﻿namespace Asilifelis.Models.View;
+
+public record ImageView(Uri Url, bool Sensitive = false, string? Name = null, string Type = "Image");
